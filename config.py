271d7599989959b38632f9f3c0a78634e26e8783
RKTP <archetype93@gmail.com>
2019-05-19 15:02:28 +0900

    [DEV] port num should not be str

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import sys
 
 DB_config = {
     'host': os.environ['dbhost'],#'news-recommendation.cvynbgusgzyb.ap-northeast-2.rds.amazonaws.com',
-    'port': os.environ['dbport'],
+    'port': int(os.environ['dbport']),
     'user':os.environ['dbusr'],
     'password': os.environ['dbpwd'],
     'db' : 'innodb'
